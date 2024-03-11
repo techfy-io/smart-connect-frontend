2b4ed9d0b623f21c940304268b5f4f1ba2c05905
Zahidkhan816 <zahidkhan717816@gmail.com>
2024-03-11 20:41:38 +0500

    update settings

--- src/Components/Dashboard/UserSetting.jsx
+++ src/Components/Dashboard/UserSetting.jsx
@@ -1,38 +1,141 @@
-import React, { useState } from 'react';
-import { Layout, Card, Button, Input } from 'antd';
-import { DeleteOutlined, EditOutlined } from '@ant-design/icons';
+import React, { useState, useEffect } from 'react';
+import { Layout, Input, Button, Form, message } from 'antd';
+import axios from 'axios';
 import './UserSetting.scss';
+
 const { Header } = Layout;
 
 const UserSetting = () => {
+    const [form] = Form.useForm();
+    const [loading, setLoading] = useState(false);
+
+    const accessToken = localStorage.getItem('accessToken');
+
+    useEffect(() => {
+        axios.get('http://smart-connect.eu-west-3.elasticbeanstalk.com/api/user/settings/', {
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+            }
+        })
+            .then(response => {
+                const { data } = response;
+                form.setFieldsValue({
+                    first_name: data.first_name,
+                    last_name: data.last_name,
+                    company: data.company,
+                    mobile: data.mobile,
+                    email: data.email,
+
+                });
+            })
+            .catch(error => {
+                console.log("Error:", error);
+            });
+    }, [form, accessToken]);
+
+
+    const handleSubmit = async (values) => {
+        setLoading(true);
+        try {
+            await axios.put('http://smart-connect.eu-west-3.elasticbeanstalk.com/api/user/settings/', values, {
+                headers: {
+                    'Authorization': `Bearer ${accessToken}`
+                }
+            });
+            message.success("Settings updated successfully!");
+        } catch (error) {
+            console.log("Error:", error);
+            message.error("failed to update user settings")
+        } finally {
+            setLoading(false);
+        }
+    };
+
     return (
-        <>
-            <div className='setting-main-layout'>
-                <div className='setting-custom-card'>
-                    <h2 className='setting-heading'>Setting</h2>
-                    <div className='setting-tab-container'>
-                        <button type='primary'
-                            className={'button-style active-button-style'}
+        <div className='setting-main-layout'>
+            <div className='setting-custom-card'>
+                <h2 className='setting-heading'>Setting</h2>
+                <div className='setting-tab-container'>
+                    <button type='primary' className={'button-style active-button-style'}>
+                        Profile
+                    </button>
+                </div>
+                <div className="setting-tab-content">
+                    <Form
+                        form={form}
+                        name="setting-profile-form"
+                        onFinish={handleSubmit}
+                        layout="vertical"
+                        initialValues={{
+                            remember: true,
+                        }}
+                    >
+                        <Form.Item
+                            name="first_name"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Please input your first name!',
+                                },
+                            ]}
                         >
-                            Profile
-                        </button>
-                    </div>
-                    <div className="setting-tab-content">
-                        <form className="setting-profile-form">
-                            <Input type="email" id="signupEmail" className="setting-form-input" placeholder="Your email" />
-                            <Input id="signupUsername" className="setting-form-input" placeholder="Your username" />
-                            <Input.Password id="signupPassword" className="setting-form-input" placeholder="Password" />
-                            <Input.Password id="confirmPassword" className="setting-form-input" placeholder="Confirm Password" />
-                            <button type="primary" htmlType="submit" className="setting-form-button">
+                            <Input placeholder="First Name" />
+                        </Form.Item>
+                        <Form.Item
+                            name="last_name"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Please input your last name!',
+                                },
+                            ]}
+                        >
+                            <Input placeholder="Last Name" />
+                        </Form.Item>
+                        <Form.Item
+                            name="email"
+                            rules={[
+                                {
+                                    required: true,
+                                    type: 'email',
+                                    message: 'Please input a valid email!',
+                                },
+                            ]}
+                        >
+                            <Input placeholder="Email" />
+                        </Form.Item>
+                        <Form.Item
+                            name="mobile"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Please input your phone number!',
+                                },
+                            ]}
+                        >
+                            <Input placeholder="Phone" />
+                        </Form.Item>
+                        <Form.Item
+                            name="company"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Please input your company!',
+                                },
+                            ]}
+                        >
+                            <Input placeholder="Company" />
+                        </Form.Item>
+                        <Form.Item>
+                            <Button type="primary" htmlType="submit" className="setting-form-button" onClick={handleSubmit} loading={loading}>
                                 Save
-                            </button>
-                        </form>
-                    </div>
+                            </Button>
+                        </Form.Item>
+                    </Form>
                 </div>
             </div>
+        </div>
+    );
+};
 
-        </>
-    )
-}
-
-export default UserSetting
+export default UserSetting;
