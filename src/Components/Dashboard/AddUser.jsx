c9ec139dc7ae6b15e8f68e156b20fe3e3b1ce0fb
Zahidkhan816 <zahidkhan717816@gmail.com>
2024-03-14 17:17:59 +0500

    update add user apis

--- src/Components/Dashboard/AddUser.jsx
+++ src/Components/Dashboard/AddUser.jsx
@@ -47,6 +47,7 @@ const AddUser = ({ isModalVisible, modalHideShow }) => {
     return (
         <Modal
             title="Add User"
+            width={450}
             open={isModalVisible}
             onCancel={handleCancel}
             footer={[
@@ -63,27 +64,97 @@ const AddUser = ({ isModalVisible, modalHideShow }) => {
                 layout="vertical"
                 onFinish={onFinish} // Callback function when form is submitted
             >
+                <div style={{ display: "flex", justifyContent: "space-between" }}>
+                    <Form.Item
+                        label="First Name*"
+                        name="firstname"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please enter a first name',
+                            },
+                        ]}
+                    >
+                        <Input />
+                    </Form.Item>
+                    <Form.Item
+                        label="Last Name*"
+                        name="lastname"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please enter a last name',
+                            },
+                        ]}
+                    >
+                        <Input />
+                    </Form.Item>
+                </div>
+                <div style={{ display: "flex", justifyContent: "space-between" }}>
+                    <Form.Item
+                        label="Company*"
+                        name="company"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please enter an company',
+                            }
+                        ]}
+                    >
+                        <Input />
+                    </Form.Item>
+                    <Form.Item
+                        label="Job Title"
+                        name="job_title"
+                    >
+                        <Input />
+                    </Form.Item>
+                </div>
+                <div style={{ display: "flex", justifyContent: "space-between" }}>
+                    <Form.Item
+                        label="Zip Code"
+                        name="zip_code"
+                    >
+                        <Input />
+                    </Form.Item>
+                    <Form.Item
+                        label="Postal Address"
+                        name="postal_address"
+                    >
+                        <Input />
+                    </Form.Item>
+                </div>
+                <div style={{ display: "flex", justifyContent: "space-between" }}>
+                    <Form.Item
+                        label="Country"
+                        name="country"
+                    >
+                        <Input />
+                    </Form.Item>
+                    <Form.Item
+                        label="City"
+                        name="city"
+                    >
+                        <Input />
+                    </Form.Item>
+                </div>
+                <div style={{ display: "flex", justifyContent: "space-between" }}>
+                    <Form.Item
+                        label="Facebook"
+                        name="facebook"
+                    >
+                        <Input />
+                    </Form.Item>
+                    <Form.Item
+                        label="Instagram"
+                        name="instagram"
+                    >
+                        <Input />
+                    </Form.Item>
+                </div>
                 <Form.Item
-                    label="First Name*"
-                    name="firstname"
-                    rules={[
-                        {
-                            required: true,
-                            message: 'Please enter a first name',
-                        },
-                    ]}
-                >
-                    <Input />
-                </Form.Item>
-                <Form.Item
-                    label="Last Name*"
-                    name="lastname"
-                    rules={[
-                        {
-                            required: true,
-                            message: 'Please enter a last name',
-                        },
-                    ]}
+                    label="Linkedin"
+                    name="linkedin"
                 >
                     <Input />
                 </Form.Item>
@@ -102,18 +173,6 @@ const AddUser = ({ isModalVisible, modalHideShow }) => {
                     ]}
                 >
                     <Input />
-                    </Form.Item>
-                <Form.Item
-                    label="company*"
-                    name="company"
-                    rules={[
-                        {
-                            required: true,
-                            message: 'Please enter an company',
-                        }
-                    ]}
-                >
-                    <Input />
                 </Form.Item>
                 <Form.Item
                     label="Phone*"
@@ -124,21 +183,35 @@ const AddUser = ({ isModalVisible, modalHideShow }) => {
                             message: 'Please enter a phone number',
                         },
                         {
-                            pattern: /\(\d{3}\) \d{3}-\d{4}/,
+                            pattern: /\+\d{2} \d{1,2} \d{2} \d{2} \d{2} \d{2}/,
                             message: 'Invalid phone number format',
                         },
                     ]}
                 >
                     <InputMask
-                        style={{ width: "100%", height: "30px", borderRadius: "5px", border: "1px solid #d9d9d9", paddingLeft: "8px", color: "black", transition: "border-color 0.3s", }}
-                        mask="(999) 999-9999" // Define the mask format for a US phone number
+                        style={{
+                            width: "95%",
+                            height: "30px",
+                            borderRadius: "5px",
+                            border: "1px solid #d9d9d9",
+                            paddingLeft: "8px",
+                            color: "black",
+                            transition: "border-color 0.3s",
+                        }}
+                        mask="+33 9 99 99 99 99"
                         maskChar=""
-                        placeholder="(123) 345-6789"
+                        placeholder="+33 6 79 95 91 92"
                     >
                     </InputMask>
                 </Form.Item>
+                <Form.Item
+                    label="Biography"
+                    name="biography"
+                >
+                    <Input />
+                </Form.Item>
             </Form>
-        </Modal>
+        </Modal >
     );
 };
 
--- src/Components/SuperAdmin/CompanyUsers.jsx
+++ src/Components/SuperAdmin/CompanyUsers.jsx
@@ -1,26 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { DeleteOutlined } from '@ant-design/icons';
 import { Spin, Button, Modal } from 'antd';
 import { Link, useNavigate, useLocation } from 'react-router-dom';
 import CompanyLogo from '../../Inspect/CompanyLogo.png';
 import Sidebar from '../Common/Sidebar';
 import './CompanyUser.scss';
-
+import AddUser from '../Dashboard/AddUser';
 const CompanyUsers = () => {
     const navigate = useNavigate();
     const location = useLocation();
     const { state } = location;
     const { company } = state || {};
+    const [isModalVisible, setIsModalVisible] = useState(false);
 
+    const modalHideShow = () => {
+        setIsModalVisible(prev => !prev);
+    };
     return (
         <div className='companyusers-container'>
             <Sidebar />
             <div className='compnayusers-content'>
                 <div className='content-header'>
                     <img className='content-header-logo' src={CompanyLogo} alt="" />
-                    {/* <div className='company-actions'>
-                        <Button type='primary' className='Add-company-btn'>Add Company</Button>
-                    </div> */}
+                    <div className='company-actions'>
+                        <Button type='primary' className='Add-company-btn' onClick={modalHideShow}>Add User</Button>
+                    </div>
                 </div>
                 <table className="table">
                     <thead>
@@ -43,6 +47,10 @@ const CompanyUsers = () => {
                     </tbody>
                 </table>
             </div>
+            <AddUser
+                isModalVisible={isModalVisible}
+                modalHideShow={modalHideShow}
+            />
         </div>
     );
 }
