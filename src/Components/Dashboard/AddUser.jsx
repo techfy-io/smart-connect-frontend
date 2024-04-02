07dda9a5d4d8b6b8d36952c1832e1a1ecc79ec35
Zahidkhan816 <zahidkhan717816@gmail.com>
2024-04-02 19:20:21 +0500

    update ui

--- src/Components/Dashboard/AddUser.jsx
+++ src/Components/Dashboard/AddUser.jsx
@@ -343,7 +343,6 @@ const AddUser = ({ isModalVisible, modalHideShow, CompaniesDate }) => {
                         </>
                     ))}
 
-{/* add another social links  */}
                     {
                         additionalSocialMediaLinks.length < 2 && (
                             <Form.Item>
