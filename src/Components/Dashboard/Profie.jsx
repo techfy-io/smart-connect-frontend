1531e0ef2912b9286e55d20947471086cec36e5c
Zahidkhan816 <zahidkhan717816@gmail.com>
2024-04-16 23:17:50 +0500

    update ui

--- src/Components/Dashboard/Profie.jsx
+++ src/Components/Dashboard/Profie.jsx
@@ -125,7 +125,14 @@ const Profile = () => {
             document.body.removeChild(link);
         });
     };
-
+    const handleSocialIconClick = (url) => {
+        if (!url) {
+            message.error("No media available please add one .");
+            // You can also use message.warning("No media link available in message.");
+        } else {
+            window.open(url, "_blank");
+        }
+    };
     return (
         <>
             {pageloading ? (
