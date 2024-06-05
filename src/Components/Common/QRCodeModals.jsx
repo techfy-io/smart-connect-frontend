45ca6f46f7d30c32da30a83f62954bde6b3c12f2
Zahidkhan816 <zahidkhan717816@gmail.com>
2024-06-05 14:55:41 +0500

    update QRcode translation and ui

--- src/Components/Common/QRCodeModals.jsx
+++ src/Components/Common/QRCodeModals.jsx
@@ -2,9 +2,11 @@ import React, { useState } from 'react';
 import { Modal, Button, Spin } from 'antd';
 import QRCode from 'react-qr-code';
 import html2canvas from 'html2canvas';
-
+import { useTranslation } from "react-i18next";
+import './QRcode.scss'
 const QRCodeModal = ({ visible, onClose, qrCodeValue, firstName }) => {
   const [downloading, setDownloading] = useState(false);
+  const { t, i18n } = useTranslation('translation');
 
   const downloadQRCode = () => {
     setDownloading(true);
@@ -20,27 +22,31 @@ const QRCodeModal = ({ visible, onClose, qrCodeValue, firstName }) => {
     });
   };
 
+  const cancelDownload=()=>{
+    setDownloading(false);
+    onClose();
+  }
   return (
     <Modal
-      width={400}
-      title={"QR Code"}
-      open={visible}
-      onCancel={onClose}
-      footer={null} // Removed the footer from here
-    >
-      <div id="qrCodeContainer" style={{ display: 'flex', justifyContent: 'center', marginBottom: '20px' }}>
-        <QRCode style={{ height: "300px" }} value={qrCodeValue} />
-      </div>
-      <div style={{ display: "flex", justifyContent: "space-between" }}> {/* Centered buttons */}
-        <Button key="close" onClick={onClose} style={{ marginLeft: '10px' }}> {/* Added margin for separation */}
-          Close
-        </Button>
-        <Button style={{ backgroundColor: "#ff8000", width: "43%" }} key="download" type="primary" onClick={downloadQRCode} disabled={downloading}>
-          {downloading ? <Spin /> : 'Download QR Code'}
-        </Button>
-
-      </div>
-    </Modal>
+    width={400}
+    title={t("QR Code")}
+    open={visible}
+    onCancel={cancelDownload}
+    footer={null} 
+  >
+    <div id="qrCodeContainer" style={{ display: 'flex', justifyContent: 'center', marginBottom: '20px' }}>
+      <QRCode style={{ height: "300px" }} value={qrCodeValue} />
+    </div>
+    <div className='download-qr-code-action-btn' > {/* Centered buttons */}
+      <Button className='qr-code-cancle-btn' key="close" onClick={cancelDownload} style={{ marginBottom: '10px' }}>
+        {t("Cancel")}
+      </Button>
+      <Button className='qr-code-download-btn'  key="download" type="primary" onClick={downloadQRCode} disabled={downloading} loading={downloading}>
+        {t('Download QR Code')}
+      </Button>
+    </div>
+  </Modal>
+  
   );
 };
 
