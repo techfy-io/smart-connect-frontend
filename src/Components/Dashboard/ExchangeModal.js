10f4312221d6bc89fd3cac525e33b5a7a913258c
Zahidkhan816 <muhammadjunaidakhter100@gmail.com>
2024-08-09 02:41:04 +0500

    fixing bug for exchange modal

--- src/Components/Dashboard/ExchangeModal.js
+++ src/Components/Dashboard/ExchangeModal.js
@@ -1,37 +1,18 @@
 import { useTranslation } from "react-i18next";
-import React, { useState } from 'react';
-import { Modal, Button } from 'antd';
-import InputMask from "react-input-mask";
-import './ExchangeModal.scss'; // Import your CSS file
+import React from 'react';
+import { Modal, Form, Input, Button } from 'antd';
+import { PatternFormat, NumericFormat } from 'react-number-format';
 
 const ExchangeModal = ({ open, onClose, onSubmit, loading }) => {
     const { t } = useTranslation('translation');
-
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
-    const [company, setCompany] = useState('');
-    const [email, setEmail] = useState('');
-    const [phoneNumber, setPhoneNumber] = useState('');
+    const [form] = Form.useForm();
 
     const handleCancel = () => {
         onClose();
-        // Reset fields if necessary
-        setFirstName('');
-        setLastName('');
-        setCompany('');
-        setEmail('');
-        setPhoneNumber('');
+        form.resetFields();
     };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const values = {
-            first_name: firstName,
-            last_name: lastName,
-            company,
-            email,
-            phone_number: phoneNumber,
-        };
+    const onFinish = async (values) => {
         onSubmit(values);
     };
 
@@ -47,82 +28,104 @@ const ExchangeModal = ({ open, onClose, onSubmit, loading }) => {
                 <Button
                     key="submit"
                     type="primary"
-                    onClick={handleSubmit}
+                    onClick={() => form.submit()}
                     loading={loading}
                 >
                     {t("Submit")}
                 </Button>,
             ]}
         >
-            <form onSubmit={handleSubmit} className="exchange-form">
-                <div className="form-item">
-                    <label>{`${t("First Name")}*`}</label>
-                    <input
-                        type="text"
+            <Form
+                name="exchangeForm"
+                form={form}
+                onFinish={onFinish}
+                layout="vertical"
+            >
+                <Form.Item
+                    label={`${t("First Name")}*`}
+                    name="first_name"
+                    rules={[
+                        {
+                            required: true,
+                            message: t('Please input your first name!'),
+                        },
+                    ]}
+                >
+                    <Input
                         maxLength={30}
-                        value={firstName}
-                        onChange={(e) => setFirstName(e.target.value)} 
-                        required 
-                        autoComplete="given-name"
-                        className="form-input"
+                        autoComplete="first_name" 
                     />
-                </div>
-                <div className="form-item">
-                    <label>{`${t("Last Name")}*`}</label>
-                    <input
-                        type="text"
+                </Form.Item>
+                <Form.Item
+                    label={`${t("Last Name")}*`}
+                    name="last_name"
+                    rules={[
+                        {
+                            required: true,
+                            message: t('Please input your last name!'),
+                        },
+                    ]}
+                >
+                    <Input
                         maxLength={30}
-                        value={lastName}
-                        onChange={(e) => setLastName(e.target.value)} 
-                        required 
-                        autoComplete="family-name"
-                        className="form-input"
+                        autoComplete="last_name" 
                     />
-                </div>
-                <div className="form-item">
-                    <label>{t("Company Name")}</label>
-                    <input
-                        type="text"
-                        value={company}
-                        onChange={(e) => setCompany(e.target.value)} 
-                        autoComplete="organization" 
-                        className="form-input"
+                </Form.Item>
+                <Form.Item
+                    label={t("Company Name")}
+                    name="company"
+                >
+                    <Input
+                        autoComplete="company" 
                     />
-                </div>
-                <div className="form-item">
-                    <label>{`${t("Email")}*`}</label>
-                    <input
-                        type="email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)} 
-                        required 
-                        autoComplete="email"
-                        className="form-input"
+                </Form.Item>
+                <Form.Item
+                    label={`${t("Email")}*`}
+                    name="email"
+                    rules={[
+                        {
+                            type: 'email',
+                            message: t('Please input a valid email!'),
+                        },
+                        {
+                            required: true,
+                            message: t('Please enter an email'),
+                        },
+                    ]}
+                >
+                    <Input
+                        autoComplete="email" 
                     />
-                </div>
-                <div className="form-item">
-                    <label>{`${t("Phone")}*`}</label>
-                    {/* <InputMask
-                        mask="+33 9 99 99 99 99"
-                        maskChar=""
+                </Form.Item>
+                <Form.Item
+                    label={`${t("Phone")}*`}
+                    name="phone_number"
+                    rules={[
+                        {
+                            required: true,
+                            message: t('Please enter a phone number'),
+                        },
+                    ]}
+                >
+                    <PatternFormat
+                        format="+33 # ## ## ## ##"
+                        allowEmptyFormatting
+                        mask="_"
                         placeholder="+33 1 23 45 67 89"
-                        value={phoneNumber}
-                        onChange={(e) => setPhoneNumber(e.target.value)} 
-                        required 
-                        autoComplete="tel"
-                        className="form-input"
-                    /> */}
-                     <input
-                        type="phone"
-                        placeholder="+33 1 23 45 67 89"
-                        value={phoneNumber}
-                        onChange={(e) => setPhoneNumber(e.target.value)} 
-                        required 
-                        autoComplete="phone"
-                        className="form-input"
+                        data-cy="phone"
+                        style={{
+                            width: "100%",
+                            height: "30px",
+                            borderRadius: "5px",
+                            border: "1px solid #d9d9d9",
+                            paddingLeft: "8px",
+                            color: "black",
+                            transition: "border-color 0.3s",
+                        }}
                     />
-                </div>
-            </form>
+                </Form.Item>
+            
+            </Form>
         </Modal>
     );
 };
